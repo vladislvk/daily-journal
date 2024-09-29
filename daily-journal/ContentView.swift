f0cd9123bc9371f0e9cccc80a1832981bce9d8ad
vladislvk <vladislavkim566@gmail.com>
2024-09-29 22:08:17 +0500

    feat:blank

--- daily-journal/ContentView.swift
+++ daily-journal/ContentView.swift
@@ -1,14 +1,11 @@
 import SwiftUI
 
 struct ContentView: View {
-    var dogs = ["bobik","aktos","sharik"]
-    
     var body: some View {
-        List(dogs, id: \.self){ listedDog in
-            Text(listedDog)
+       JournalEntitiesListView()
         }
-    }
 }
+
 
 #Preview {
     ContentView()
