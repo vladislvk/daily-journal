9aa74a52f8c9cdc290bfe331dc90c5f45679fa0a
vladislvk <vladislavkim566@gmail.com>
2024-09-29 21:56:08 +0500

    feat:blank

--- daily-journal/JournalEntitiesListView.swift
+++ daily-journal/JournalEntitiesListView.swift
@@ -1,12 +1,10 @@
 import SwiftUI
 
 struct JournalEntitiesListView: View {
-    let journalEntities:[JournalEntity] = [JournalEntity(title: "Iphone 16"),JournalEntity(title: "Iphone 15"),
-                                           JournalEntity(title: "Iphone 14"),JournalEntity(title: "Iphone 13")]
     var body: some View {
         NavigationStack{
             List(journalEntities){listedJournalEntity in
-                NavigationLink(destination: Circle()){
+                NavigationLink(destination: Text(listedJournalEntity.text)){
                         Text(listedJournalEntity.title)
                 }
                 
--- daily-journal/JournalEntity.swift
+++ daily-journal/JournalEntity.swift
@@ -2,8 +2,21 @@ import SwiftUI
 
 class JournalEntity: Identifiable {
     var title:String=""
-    init(title: String) {
+    var text:String=""
+    var rating:Int=0
+    var date:Date=Date()
+    
+    
+    init(title: String, text: String, rating: Int, date: Date) {
         self.title = title
+        self.text = text
+        self.rating = rating
+        self.date = date
     }
 }
 
+
+let journalEntities:[JournalEntity] =
+[JournalEntity(title: "zxc", text: "zxc", rating: 5, date: Date()),
+ JournalEntity(title: "zxc", text: "zxc", rating: 5, date: Date()),
+ JournalEntity(title: "zxc", text: "zxc", rating: 5, date: Date())]
